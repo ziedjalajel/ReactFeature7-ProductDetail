605027a23a084e295a35e1778e4c2bf5e77c754b
Lailz <lailaabdulraheem@gmail.com>
2020-07-09 15:09:40 +0300

    add search

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Styling
+import { SearchBarStyled } from "../styles";
+
+const SearchBar = (props) => {
+  return (
+    <SearchBarStyled
+      onChange={(event) => props.setQuery(event.target.value)}
+      placeholder="Search for a cookie name"
+    />
+  );
+};
+
+export default SearchBar;
