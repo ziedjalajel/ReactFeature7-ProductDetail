7633f4c340365f57ba186a01dc97a8d4ae5242e7
Lailz <lailaabdulraheem@gmail.com>
2020-07-09 19:40:10 +0300

    cleanup

--- src/components/CookieDetail.js
+++ src/components/CookieDetail.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { DeleteButtonStyled, DetailWrapper } from "../styles";
+import { DetailWrapper } from "../styles";
 
 const CookieDetail = (props) => {
   const cookie = props.cookie;
-
-  const handleDelete = () => {
-    props.deleteCookie(cookie.id);
-  };
-
   return (
     <DetailWrapper>
       <h1>{cookie.name}</h1>
       <img src={cookie.image} alt={cookie.name} />
       <p>{cookie.description}</p>
       <p>{cookie.price} KD</p>
-      <DeleteButtonStyled onClick={handleDelete}>Delete</DeleteButtonStyled>
     </DetailWrapper>
   );
 };
